97fa069aaf8fe2796a0d34b363a815af214b3169
Steve <steve.suissa@epita.fr>
2020-01-28 13:37:39 +0100

    Ajout de Photon et du Lobby

---
+++ DeadlyScience/Assets/Scripts/Réseau/DDOL.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class DDOL : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+}
---
+++ DeadlyScience/Assets/Scripts/Réseau/PlayerNetwork.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class PlayerNetwork : MonoBehaviour
+{
+    public static PlayerNetwork Instance;
+    public string PlayerName { get; private set; }
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        Instance = this;
+
+        PlayerName = "Distul#" + Random.Range(1000, 9999);
+    }
+
+}
