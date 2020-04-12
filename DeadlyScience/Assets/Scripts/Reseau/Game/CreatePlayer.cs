04f25f32e2fa0e7188045f08d47eb3b70fb3b392
Steve Suissa <steve.suissa@epita.fr>
2020-04-12 18:09:55 +0200

    Test sur le CreatePlayer

--- DeadlyScience/Assets/Scripts/Reseau/Game/CreatePlayer.cs
+++ DeadlyScience/Assets/Scripts/Reseau/Game/CreatePlayer.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using Photon.Chat;
 using Photon.Pun;
 using UnityEngine;
 
@@ -9,14 +10,47 @@ namespace ds
     public class CreatePlayer : MonoBehaviour
     {
         private PhotonView PV;
+        private Photon.Realtime.Player _player = PhotonNetwork.LocalPlayer;
+
 
         void Start()
         {
-            PV = GetComponent<PhotonView>();
+            string[] tableau = new string[4];
+            int i = 0;
+            //Determine which position to spawn each player
+            foreach (var roomPlayer in PhotonNetwork.CurrentRoom.Players)
+            {
+                tableau[i] = roomPlayer.Value.UserId;
+                i++;
+            }
+            print(_player.UserId);
+            print("BLANK");
+            print(tableau[0]);
+            print(tableau[1]);
+
+            if (tableau[0] == _player.UserId)
+            {
+                var player = PhotonNetwork.Instantiate(Path.Combine("Prefabs", "Player"), new Vector3(-5, 5, 6), Quaternion.identity, 0);
+                player.GetComponent<PlayerNetwork>().isLocal = true;
+            }
+            else if (tableau[1] == _player.UserId)
+            {
+                var player = PhotonNetwork.Instantiate(Path.Combine("Prefabs", "Player"), new Vector3(-5, 5, 4), Quaternion.identity, 0);
+                player.GetComponent<PlayerNetwork>().isLocal = true;
+            }
+            else if (tableau[2] == _player.UserId)
+            {
+                var player = PhotonNetwork.Instantiate(Path.Combine("Prefabs", "Player"), new Vector3(-5, 5, 2), Quaternion.identity, 0);
+                player.GetComponent<PlayerNetwork>().isLocal = true;
+            }
+            else
+            {
+                var player = PhotonNetwork.Instantiate(Path.Combine("Prefabs", "Player"), new Vector3(-5, 5, 0), Quaternion.identity, 0);
+                player.GetComponent<PlayerNetwork>().isLocal = true;
+            }
+            
 
             // TODO : LEANDRE : Position
-            var player = PhotonNetwork.Instantiate(Path.Combine("Prefabs", "Player"), new Vector3(-5, 5, 6), Quaternion.identity, 0);
-            player.GetComponent<PlayerNetwork>().isLocal = true;
 
             print("Le joueur a été instancié");
         }
