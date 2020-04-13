0f199d3f6f7532ef9be99f878a26e69f55966e27
Steve Suissa <steve.suissa@epita.fr>
2020-04-13 16:08:40 +0200

    Fixed Network ?

--- DeadlyScience/Assets/Scripts/Reseau/Game/CreatePlayer.cs
+++ DeadlyScience/Assets/Scripts/Reseau/Game/CreatePlayer.cs
@@ -9,39 +9,20 @@ namespace ds
 {
     public class CreatePlayer : MonoBehaviour
     {
-        private PhotonView PV;
-        private Photon.Realtime.Player player = PhotonNetwork.LocalPlayer;
-
-
         void Start()
         {
-            PV = GetComponent<PhotonView>();
-            int[] tableau = new int[4];
-            int i = 0;
-            int id = player.ActorNumber;
-            //Determine which position to spawn each player
-            foreach (var roomPlayer in PhotonNetwork.CurrentRoom.Players)
-            {
-                tableau[i] = roomPlayer.Value.ActorNumber;
-                i++;
-            }
-            
-            print(id);
-            print("BLANK");
-            print(tableau[0]);
-            print(tableau[1]);
-
-            if (tableau[0] == id)
+            int countofplayers = PhotonNetwork.CountOfPlayers;
+            if (countofplayers % 4 == 0)
             {
                 var _player = PhotonNetwork.Instantiate(Path.Combine("Prefabs", "Player"), new Vector3(-5, 5, 6), Quaternion.identity, 0);
                 _player.GetComponent<PlayerNetwork>().isLocal = true;
             }
-            else if (tableau[1] == id)
+            else if (countofplayers % 4 == 1)
             {
                 var _player = PhotonNetwork.Instantiate(Path.Combine("Prefabs", "Player"), new Vector3(-5, 5, 4), Quaternion.identity, 0);
                 _player.GetComponent<PlayerNetwork>().isLocal = true;
             }
-            else if (tableau[2] == id)
+            else if (countofplayers % 4 == 2)
             {
                 var _player = PhotonNetwork.Instantiate(Path.Combine("Prefabs", "Player"), new Vector3(-5, 5, 2), Quaternion.identity, 0);
                 _player.GetComponent<PlayerNetwork>().isLocal = true;
