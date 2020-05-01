4415231089ca1420eec2de3aeafcce968aeac11d
Celian Raimbault <celian.raimbault@epita.fr>
2020-05-01 11:46:55 +0200

    Moved audio sample

---
+++ DeadlyScience/Assets/Scripts/Game/AudioSample.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ds
+{
+    public class AudioSample : MonoBehaviour
+    {
+        public float volume = 0;
+        public float Volume
+        {
+            get => volume;
+            set
+            {
+                GetComponent<AudioSource>().volume = value * Audio.musicVolume;
+            }
+        }
+
+        private void Update()
+        {
+            Volume = volume;
+        }
+    }
+}
