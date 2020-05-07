7ac02e81f63ba2db1866228270939022df14da11
Steve Suissa <steve.suissa@epita.fr>
2020-05-07 23:36:25 +0200

    Resolved the bug of player spawning on top of another player

--- DeadlyScience/Assets/Scripts/Reseau/Game/CreatePlayer.cs
+++ DeadlyScience/Assets/Scripts/Reseau/Game/CreatePlayer.cs
@@ -7,11 +7,26 @@ using UnityEngine;
 
 namespace ds
 {
-    public class CreatePlayer : MonoBehaviour
+    public class CreatePlayer : MonoBehaviourPunCallbacks
     {
         void Start()
         {
-            int coorp = CreateRoomMenu.where[((int)PhotonNetwork.CountOfPlayers) % 4];
+            bool not_found = true;
+            Photon.Realtime.Player player = PhotonNetwork.LocalPlayer;
+            int i = 0;
+            while (not_found && i < PhotonNetwork.PlayerList.Length)
+            {
+                if (PhotonNetwork.PlayerList[i] == player)
+                {
+                    not_found = false;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            int coorp = CreateRoomMenu.where[i];
+            //int coorp = CreateRoomMenu.where[PhotonNetwork.CountOfPlayers % 4];
             PhotonNetwork.Instantiate(Path.Combine("Prefabs", "Attente"), new Vector3((float) (4 * (coorp % CreateRoomMenu.Xm) + 2.5), (float) 3.75, (float) (4 * (coorp / CreateRoomMenu.Xm) + 2.5)), Quaternion.identity, 0);
             var _player = PhotonNetwork.Instantiate(Path.Combine("Prefabs", "Player"), 
                 new Vector3((float) (4 * (coorp % CreateRoomMenu.Xm) + 2.5), 3, (float) (4 * (coorp / CreateRoomMenu.Xm) + 2.5)),
@@ -19,7 +34,7 @@ namespace ds
             _player.GetComponent<PlayerNetwork>().isLocal = true;
             if (PhotonNetwork.IsMasterClient)
             {
-                int g = CreateRoomMenu.PlayerNumber-1;
+                int g = CreateRoomMenu.PlayerNumber -1;
                 if (g == 0)
                 {
                     g = 1;
@@ -30,7 +45,7 @@ namespace ds
                 {
                     g -= 1;
                     print(CreateRoomMenu.where[g + 4]);
-                    var newCube = PhotonNetwork.Instantiate(Path.Combine("Prefabs", "Serum"),
+                    PhotonNetwork.Instantiate(Path.Combine("Prefabs", "Serum"),
                         new Vector3((float) (4 * (CreateRoomMenu.where[g+4] % CreateRoomMenu.Xm) + 2.5), 2, (float) (4 * (CreateRoomMenu.where[g+4] / CreateRoomMenu.Xm) + 2.5)),
                         new Quaternion(0, 0, 0, 0));
                 }
