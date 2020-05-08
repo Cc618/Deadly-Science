71b6d4aca2954aefbede152c8e31e1986a91f1c1
Steve Suissa <steve.suissa@epita.fr>
2020-05-08 10:28:16 +0200

    Simplified CreatePlayer script and resolved the bug of clipping on the floor

--- DeadlyScience/Assets/Scripts/Reseau/Game/CreatePlayer.cs
+++ DeadlyScience/Assets/Scripts/Reseau/Game/CreatePlayer.cs
@@ -9,14 +9,52 @@ namespace ds
 {
     public class CreatePlayer : MonoBehaviourPunCallbacks
     {
+        private PhotonView pv;
         void Start()
         {
+            pv = GetComponent<PhotonView>();
+            if (pv.IsMine)
+            {
+                if (PhotonNetwork.IsMasterClient)
+                {
+                    int coorp = CreateRoomMenu.where[0];
+                    PhotonNetwork.Instantiate(Path.Combine("Prefabs", "Attente"), new Vector3((float) (4 * (coorp % CreateRoomMenu.Xm) + 2.5), (float) 3.75, (float) (4 * (coorp / CreateRoomMenu.Xm) + 2.5)), Quaternion.identity, 0);
+                    var player = PhotonNetwork.Instantiate(Path.Combine("Prefabs", "Player"), 
+                        new Vector3((float) (4 * (coorp % CreateRoomMenu.Xm) + 2.5), 3, (float) (4 * (coorp / CreateRoomMenu.Xm) + 2.5)),
+                        Quaternion.identity, 0);
+                    player.GetComponent<PlayerNetwork>().isLocal = true;
+                    print("Le MasterClient a été instancié");
+                    
+                    int g = CreateRoomMenu.PlayerNumber -1;
+                    if (g == 0)
+                    {
+                        g = 1;
+                    }
+
+                    print("Génération des sérums :");
+                    while (g > 0)
+                    {
+                        g -= 1;
+                        print(CreateRoomMenu.where[g + 4]);
+                        PhotonNetwork.Instantiate(Path.Combine("Prefabs", "Serum"),
+                            new Vector3((float) (4 * (CreateRoomMenu.where[g+4] % CreateRoomMenu.Xm) + 2.5), 2, (float) (4 * (CreateRoomMenu.where[g+4] / CreateRoomMenu.Xm) + 2.5)),
+                            new Quaternion(0, 0, 0, 0));
+                    }
+                    print("Fin de génération des sérums");
+                    pv.RPC("CreateOtherPlayers", RpcTarget.Others);
+                }
+            }
+        }
+
+        [PunRPC]
+        void CreateOtherPlayers()
+        {
+            Photon.Realtime.Player localplayer = PhotonNetwork.LocalPlayer;
             bool not_found = true;
-            Photon.Realtime.Player player = PhotonNetwork.LocalPlayer;
-            int i = 0;
+            int i = 1;
             while (not_found && i < PhotonNetwork.PlayerList.Length)
             {
-                if (PhotonNetwork.PlayerList[i] == player)
+                if (PhotonNetwork.PlayerList[i] == localplayer)
                 {
                     not_found = false;
                 }
@@ -26,30 +64,11 @@ namespace ds
                 }
             }
             int coorp = CreateRoomMenu.where[i];
-            //int coorp = CreateRoomMenu.where[PhotonNetwork.CountOfPlayers % 4];
             PhotonNetwork.Instantiate(Path.Combine("Prefabs", "Attente"), new Vector3((float) (4 * (coorp % CreateRoomMenu.Xm) + 2.5), (float) 3.75, (float) (4 * (coorp / CreateRoomMenu.Xm) + 2.5)), Quaternion.identity, 0);
-            var _player = PhotonNetwork.Instantiate(Path.Combine("Prefabs", "Player"), 
+            var player = PhotonNetwork.Instantiate(Path.Combine("Prefabs", "Player"), 
                 new Vector3((float) (4 * (coorp % CreateRoomMenu.Xm) + 2.5), 3, (float) (4 * (coorp / CreateRoomMenu.Xm) + 2.5)),
                 Quaternion.identity, 0);
-            _player.GetComponent<PlayerNetwork>().isLocal = true;
-            if (PhotonNetwork.IsMasterClient)
-            {
-                int g = CreateRoomMenu.PlayerNumber -1;
-                if (g == 0)
-                {
-                    g = 1;
-                }
-
-                print("Sérums :");
-                while (g > 0)
-                {
-                    g -= 1;
-                    print(CreateRoomMenu.where[g + 4]);
-                    PhotonNetwork.Instantiate(Path.Combine("Prefabs", "Serum"),
-                        new Vector3((float) (4 * (CreateRoomMenu.where[g+4] % CreateRoomMenu.Xm) + 2.5), 2, (float) (4 * (CreateRoomMenu.where[g+4] / CreateRoomMenu.Xm) + 2.5)),
-                        new Quaternion(0, 0, 0, 0));
-                }
-            }
+            player.GetComponent<PlayerNetwork>().isLocal = true;
             print("Le joueur a été instancié");
         }
     }
