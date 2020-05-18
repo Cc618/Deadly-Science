abee07596487c14bbed06a5064552d01ee200bf8
Leandre PERROT <leandre.perrot@epita.fr>
2020-05-18 18:41:13 +0200

    Sérum d'Urgence Opérationnel

--- DeadlyScience/Assets/Scripts/Player/PlayerNetwork.cs
+++ DeadlyScience/Assets/Scripts/Player/PlayerNetwork.cs
@@ -216,14 +216,21 @@ namespace ds
                     PlayerMaster.UpdateRevengePlayers(id);
             }
         }
+        
+        public IEnumerator SerumUrgence()
+        {
+            yield return new WaitForSeconds(5);
+            SendSetStatus(PlayerState.PlayerStatus.HEALED);
+        }
 
         public void SendSetStatus(PlayerState.PlayerStatus status)
         {
             if (status == PlayerState.PlayerStatus.REVENGE && Player.alterations[8])
             {
-                Player.alterations[9] = false;
+                Player.alterations[8] = false;
                 AffichagePowerUpJoueur.MaJ(Player.alterations);
                 SendSetStatus(PlayerState.PlayerStatus.GHOST);
+                StartCoroutine(SerumUrgence());
             }
             else
             {
